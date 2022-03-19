cb832ad04dfbbc30a4b77dc8c13c4c0f042bc2da
Computer <noice.computer@gmail.com>
2022-03-19 00:55:04 -0400

    Initial tests for MazeBuilder
    MazeBuilder needs additionally refactoring in order to be testable. May
    wait until introduction of SpringBoot before completing these tests.

--- src/main/java/com/amw/sms/mazes/MazeBuilder.java
+++ src/main/java/com/amw/sms/mazes/MazeBuilder.java
@@ -11,6 +11,9 @@ import com.amw.sms.grid.DistancesGrid;
 import com.amw.sms.grid.Grid;
 
 public class MazeBuilder {
+    private final String INVALID_SIZE_MESSAGE = "Maze cannot be created with %s %s";
+    private final String NO_GRID_SIZE = "No grid size specified.";
+
     private final Dijkstra dijk;    //Used for maze generation
     private int rowCount = 0, colCount = 0;
     private int startRow = 0, startColumn = 0; //TODO do we use defaults still? We shouldn't leave them uninitialized but we also will never use the default values...
@@ -23,22 +26,48 @@ public class MazeBuilder {
     private boolean useRandomEnd = false;
     private boolean showDistances = false;
 
+    /**
+     * Constructs new MazeBuilder.
+     */
     public MazeBuilder(){
         dijk = new Dijkstra();
         genAlgorithm = new Sidewinder();
     }
 
-    public MazeBuilder withSize(int rowCount, int colCount){
+    /**
+     * Set size of maze.
+     * @param rowCount Number of rows the maze will have.
+     * @param colCount Number of columns the maze will have.
+     * @return Builder instance
+     * @throws InvalidMazeException When row count or column count are either invalid (less than or equal to 0).
+     */
+    public MazeBuilder withSize(int rowCount, int colCount) throws InvalidMazeException{
+        if(rowCount <= 0) throw new InvalidMazeException(INVALID_SIZE_MESSAGE.formatted(rowCount, "rows"));
+        if(colCount <= 0) throw new InvalidMazeException(INVALID_SIZE_MESSAGE.formatted(colCount, "columns"));
+
         this.rowCount = rowCount;
         this.colCount = colCount;
         return this;
     }
 
+    /**
+     * Set the maze-generation algorithm to be used during the build process. This is used to generate the
+     * paths of the maze.
+     * @param genAlgorithm Maze generation algorithm to be used.
+     * @return Builder instance
+     */
     public MazeBuilder usingAlgorithm(MazeGenAlgorithm genAlgorithm){
         this.genAlgorithm = genAlgorithm;
         return this;
     }
 
+    /**
+     * Sets starting point of maze.
+     * TODO - rename.
+     * @param row Row of entrance.
+     * @param column Column of entrance.
+     * @return Builder instance.
+     */
     public MazeBuilder startFrom(int row, int column){
         this.startAtFirst = false;
 
@@ -47,6 +76,13 @@ public class MazeBuilder {
         return this;
     }
 
+    /**
+     * Sets ending point of maze.
+     * TODO - rename.
+     * @param row Row of exit.
+     * @param column Column of exit.
+     * @return Builder instance.
+     */
     public MazeBuilder endAt(int row, int column){
         this.endAtLast = false;
 
@@ -55,6 +91,11 @@ public class MazeBuilder {
         return this;
     }
 
+    /**
+     * Sets entrance and exit in maze such that the longest path (or at least, a quite long path)
+     * within the maze is used.
+     * @return Builder instance.
+     */
     public MazeBuilder usingLongestPath(){
         this.startAtFirst = false;
         this.endAtLast = false;
@@ -63,31 +104,54 @@ public class MazeBuilder {
         return this;
     }
 
+    /**
+     * Sets starting point of maze to a random cell.
+     * @return Builder instance
+     */
     public MazeBuilder usingRandomStart(){
         this.startAtFirst = false;
         this.useRandomStart = true;
         return this;
     }
 
+    /**
+     * Sets end point of maze to a random cell.
+     * @return Builder instance
+     */
     public MazeBuilder usingRandomEnd(){
         this.endAtLast = false;
         this.useRandomEnd = true;
         return this;
     }
 
+    /**
+     * TODO
+     * @return
+     */
     public MazeBuilder showDistances(){
         this.showDistances = true;
         return this;
     }
 
+    /**
+     * Builds the Maze based on the internal state set by client using builder methods.
+     * @return Fully constructed maze.
+     * @throws InvalidMazeException When grid size is not set, or when the entrance or exits are invalid. 
+     */
     public Maze build() throws InvalidMazeException{
-        final var grid = new DistancesGrid(this.rowCount, this.colCount);
-        System.out.println("-- START MAZE BUILDER -- \n Empty grid"); //todo Temp
+        //Validations
+        if(this.rowCount == 0) throw new InvalidMazeException(NO_GRID_SIZE);    //TODO - if grid size is required, does it make sense to require it as part of constructor? See builder pattern conventions
+
+        //Initial grid
+        final var grid = new DistancesGrid(this.rowCount, this.colCount); //TODO - move to factory for dependency injection??
+        System.out.println("-- START MAZE BUILDER -- \n Empty grid"); //TODO - Temp print
         System.out.println(grid); //todo Temp
 
+        //Build it out
         this.genAlgorithm.apply(grid);
         final Entry<Cell, Cell> entrances = this.getEntrances(grid);
 
+        //Show it
         if(showDistances){
             grid.setDistances(dijk.getDistances(grid, entrances.getKey()));
         }
@@ -95,6 +159,13 @@ public class MazeBuilder {
         return new Maze(grid, entrances);
     }
 
+    /**
+     * Get the entrances of the maze.
+     * TODO - rename and clean-up
+     * @param grid
+     * @return
+     * @throws InvalidMazeException
+     */
     private Entry<Cell, Cell> getEntrances(Grid grid) throws InvalidMazeException{
         Cell startCell, endCell;
 
--- src/test/java/com/amw/sms/mazes/MazeBuilderTest.java
+++ src/test/java/com/amw/sms/mazes/MazeBuilderTest.java
@@ -1,12 +1,20 @@
 package com.amw.sms.mazes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import java.util.stream.Stream;
 
 import com.amw.sms.mazes.InvalidMazeException;
 import com.amw.sms.mazes.MazeBuilder;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class MazeBuilderTest {
     /*Size tests */
@@ -21,11 +29,49 @@ public class MazeBuilderTest {
         assertEquals(6, grid.getColumnCount());
     }
 
-    @Test
-    void testBuild_whenNoSizeProvided_throwsInvalidMazeException() {
-        
+    @ParameterizedTest
+    @MethodSource("invalidRowAndColProvider")
+    void testWithSize_whenZeroRows_throwsInvalidMazeException(int rows, int columns) throws InvalidMazeException {
+        final var exception = assertThrows(InvalidMazeException.class, () -> {
+            new MazeBuilder()
+                .withSize(rows, columns)
+                .build();
+        });
+        assertNotNull(exception.getMessage());
     }
 
+    //For invalid size tests
+    static Stream<Arguments> invalidRowAndColProvider(){
+        return Stream.of(
+            arguments(0, 5),    //Zero rows
+            arguments(-1, 5),   //Negative rows
+            arguments(1, 0),    //Zero cols
+            arguments(1, -4)    //Negative cols
+        );
+    }
+
+    @Test
+    void testBuild_whenNoSizeProvided_throwsInvalidMazeException() {
+        final var exception = assertThrows(InvalidMazeException.class, () -> {
+            new MazeBuilder()
+                .build();
+        });
+        assertNotNull(exception.getMessage());
+    }
+
+    /* Setting entrances and exits */
+    @Test
+    void testStartFrom_andTestWithSize_returnsMazeThatStartsWithSpecifiedCell() throws InvalidMazeException {
+        final var maze = new MazeBuilder()
+            .withSize(5, 5)
+            .startFrom(1, 3)
+            .build();
+
+        assertTrue(maze.getStartCell().isPresent());
+        final var startCell = maze.getStartCell().get();
+        assertEquals(1, startCell.getRowPosition());
+        assertEquals(3, startCell.getColumnPosition());
+    }
 
     @Test
     void testEndAt_andTestWithSize_returnsMazeThatEndsWithSpecifiedCell() throws InvalidMazeException {
@@ -40,48 +86,63 @@ public class MazeBuilderTest {
         assertEquals(3, endCell.getColumnPosition());
     }
 
+    /* Non-set entrances and exits */
     @Test
-    void testStartFrom_andTestWithSize_returnsMazeThatEndsWithSpecifiedCell() throws InvalidMazeException {
+    void testBuild_whenNoStartProvided_returnsMazeThatStartsWithFirstCell() throws InvalidMazeException {
         final var maze = new MazeBuilder()
             .withSize(5, 5)
-            .startFrom(1, 3)
             .build();
 
         assertTrue(maze.getStartCell().isPresent());
         final var startCell = maze.getStartCell().get();
-        assertEquals(1, startCell.getRowPosition());
-        assertEquals(3, startCell.getColumnPosition());
+        assertEquals(0, startCell.getRowPosition());
+        assertEquals(0, startCell.getColumnPosition());
     }
+    
 
     @Test
-    void testShowDistances() {
+    void testBuild_whenNoEndProvided_returnsMazeWithLastCellAsExit() throws InvalidMazeException {
+        final var rowCount = 4;
+        final var colCount = 5;
         
+        final var maze = new MazeBuilder()
+            .withSize(rowCount, colCount)
+            .build();
+
+        assertTrue(maze.getEndCell().isPresent());
+        final var endCell = maze.getEndCell().get();
+        assertEquals(rowCount-1, endCell.getRowPosition());
+        assertEquals(colCount-1, endCell.getColumnPosition());
     }
 
-    @Test
-    void testUsingAlgorithm() {
-
-    }
-
-    @Test
-    void testUsingLongestPath() {
-
-    }
-
-    @Test
-    void testUsingRandomEnd() {
-
-    }
-
+    /* Random entrances and exits */
+    //Requires mocking of grid class or Random class? Both of which should be done when Spring is used.
     @Test
     void testUsingRandomStart() {
-
+        //TODO
     }
 
-
-
+    //Requires mocking of grid class or Random class? Both of which should be done when Spring is used.
     @Test
-    void testBuild_whenNoValuesProvided_returnsInstanceOfMaze() {
+    void testUsingRandomEnd() {
+        //TODO
+    }
 
+    //Requires mocking of grid class or Random class? Both of which should be done when Spring is used.
+    @Test
+    void testShowDistances() {
+        //TODO
+    }
+
+    //Requires mocking of grid class and Dijkstra class
+    @Test
+    void testUsingAlgorithm() {
+        //TODO
+    }
+
+    //Requires mocking of grid class and Dijkstra class
+    @Test
+    void testUsingLongestPath() {
+        //TODO
     }
 }
